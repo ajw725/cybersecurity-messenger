de358f4bf9ad5e6bd77221106f7221c358dac21f
Andrew Weinstein <andrew@altvia.com>
2020-10-03 19:36:51 -0600

    x-frame-options header

--- app/controllers/devise_overrides/sessions_controller.rb
+++ app/controllers/devise_overrides/sessions_controller.rb
@@ -6,9 +6,10 @@ module DeviseOverrides
     private
 
     def after_sign_in_path_for(resource_or_scope)
+      return messages_path if resource_or_scope.is_a?(User)
+
       stored_path = stored_location_for(resource_or_scope)
       return stored_path if stored_path.present?
-      return messages_path if resource_or_scope.is_a?(User)
 
       signed_in_root_path(resource_or_scope)
     end
