8d4ffc756b37bad1b5b563fd25911ec273ccebf4
Andrew Weinstein <andrew@altvia.com>
2020-10-15 18:37:40 -0600

    Revert "Switch from 587 to 465"
    This reverts commit 441b1ed6f206385bc97c82fe466214dc10666e09.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: ENV['SMTP_ADDRESS'],
-    port: 465,
+    port: 587,
     domain: 'andrewsapp.app',
     user_name: Rails.application.credentials.smtp_username,
     password: Rails.application.credentials.smtp_password,
